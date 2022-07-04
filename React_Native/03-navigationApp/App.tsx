206edb7097f8b4ce1cdc090ba2ee3e05975f17ef
Gilberto Acosta <acostagilberto95@gmail.com>
2022-07-04 17:02:07 -0400

    update

--- React_Native/03-navigationApp/App.tsx
+++ React_Native/03-navigationApp/App.tsx
@@ -1,12 +1,14 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import StackNavigator from './src/navigation/StackNavigator';
+// import StackNavigator from './src/navigation/StackNavigator';
+import { BasicSideMenu } from './src/navigation/BasicSideMenu';
 
 const App = () => {
 	return (
 		<NavigationContainer>
-			<StackNavigator />
+			{/* <StackNavigator /> */}
+			<BasicSideMenu/>
 		</NavigationContainer>
 	);
 };
---
+++ React_Native/03-navigationApp/src/navigation/BasicSideMenu.tsx
@@ -0,0 +1,14 @@
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import SettingScreen from '../screens/SettingScreen';
+import StackNavigator from './StackNavigator';
+
+const Drawer = createDrawerNavigator();
+
+export const BasicSideMenu= () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="StackNavigator" component={StackNavigator} />
+      <Drawer.Screen name="SettingScreen" component={SettingScreen} />
+    </Drawer.Navigator>
+  );
+}
