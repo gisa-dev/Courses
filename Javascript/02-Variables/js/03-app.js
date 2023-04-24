53bdec600d189376809e5ad50e9678413827e259
Gilbert Acosta <gisawebdev@gmail.com>
2023-04-24 16:10:54 -0400

    String Methods

---
+++ Javascript/02-Variables/js/03-app.js
@@ -0,0 +1,10 @@
+const producto = 'Tablet';
+
+
+// no se pueden reasignar
+// producto = 'Monitor';
+
+
+console.log(producto);
+
+const precio = 20;
---
+++ Javascript/03-Strings/js/02-app.js
@@ -0,0 +1,9 @@
+const producto = 'Monitor 20 pulgadas';
+
+console.log(producto);
+
+// Conocer la cantidad de letras de la cadena de texto
+console.log(producto.length);
+
+console.log(producto.indexOf('Monitor'));
+console.log(producto.includes('Monitor'));
