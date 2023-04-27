3b51bf65a1f7269d55128686631c391a4a245631
Gilbert Acosta <gisawebdev@gmail.com>
2023-04-27 16:13:10 -0400

    update

---
+++ Dart/01_basico/02_datatypes.dart
@@ -0,0 +1,27 @@
+main() {
+  // ==== Numeros
+  int a = 10;
+  double b = 5.5;
+
+  int? c;
+
+  int _a = 30;
+  double $b = 40;
+
+  double resultado = _a + $b;
+
+  // print(resultado);
+
+  // ==== String
+
+  String nombre = 'Tony';
+  String nombre2 = "Tony";
+  String nombre3 = "O'Connor";
+
+  String multilinea = '''
+  Hola Mundo
+  Como estas?
+  O'Connor''';
+
+  print(multilinea);
+}
