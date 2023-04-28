ab6ecff5141585fb41187bdf84670c35f33a70a4
Gilbert Acosta <gisawebdev@gmail.com>
2023-04-28 14:54:22 -0400

    update numbers

--- Javascript/03-Strings/js/05-app.js
+++ Javascript/03-Strings/js/05-app.js
@@ -1,5 +1,23 @@
+
+// replace
 const producto = 'Monitor 20 pulgadas';
 console.log(producto);
 console.log(producto.replace('pulgadas', '"'));
 console.log(producto.replace('Monitor', 'Monitor curvo'));
 
+
+
+// slice
+console.log(producto.slice(0,10));
+// console.log(producto.slice(2,1));
+
+
+// Aternativa a slice
+
+console.log(producto.substring(0,10));
+console.log(producto.substring(2,1));
+
+
+const usuario = 'Juan';
+console.log(usuario.substring(0,1));
+console.log(usuario.charAt(0));
---
+++ Javascript/03-Strings/js/06-app.js
@@ -0,0 +1,19 @@
+const producto = 'Monitor 20 pulgadas';
+
+// repeat
+
+const texto = 'en promoción'.repeat(2);
+
+console.log(texto);
+console.log(`${producto} ${texto} !!!`);
+
+// Split
+
+const actividad = 'Estoy aprendiendo JavaScript Moderno';
+console.log(actividad.split(' '));
+
+const hobbies = 'Leer, caminar, escuchar más musica, escribir, aprender a programar';
+console.log(hobbies.split(', '));
+
+const tweet = 'Aprendiendo JavaScript #JSModernoConJuan';
+console.log(tweet.split('#'));
