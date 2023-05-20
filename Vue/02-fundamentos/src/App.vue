934ef3bd24b2e2002d6671711663a7a24bcf30cf
Gilbert Acosta <gisawebdev@gmail.com>
2023-05-20 19:47:57 -0400

    Update to vite

---
+++ Vue/02-fundamentos/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Counter from './components/Counter.vue';
+</script>
+
+<template>
+	<img
+		alt="Vue logo"
+		class="logo"
+		src="./assets/logo.svg"
+		width="150"
+		height="150"
+	/>
+
+	<Counter :start="100" />
+</template>
+
