6a11cb5d4bdfb98e1428781d60e55043fc7b46a6
Gilbert Acosta <gisawebdev@gmail.com>
2023-06-21 16:09:27 -0400

    update

---
+++ Javascript/13-DOM/js/02-scripts.js
@@ -0,0 +1,6 @@
+let header = document.getElementsByClassName('header');
+console.log(header);
+let hero = document.getElementsByClassName('hero');
+console.log(hero);
+let contenedor = document.getElementsByClassName('contenedor');
+console.log(contenedor);
