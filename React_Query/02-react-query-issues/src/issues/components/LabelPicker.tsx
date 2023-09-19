eaab640795105e4abafaff0e118809adc146a64e
Gilbert Acosta <gisawebdev@gmail.com>
2023-09-19 19:14:56 -0400

    update

---
+++ React_Query/02-react-query-issues/src/issues/components/LabelPicker.tsx
@@ -0,0 +1,23 @@
+import {useLabels} from '../hooks/useLabels';
+
+export const LabelPicker = () => {
+	const labelQuery = useLabels();
+
+	if (labelQuery.isLoading) {
+		return <h1>Loading...</h1>;
+	}
+
+	return (
+		<div>
+			{labelQuery.data?.map((label) => (
+				<span
+					key={label.id}
+					className='badge rounded-pill m-1 label-picker'
+					style={{border: `1px solid #${label.color}`, color: `#${label.color}`}}
+				>
+					{label.name}
+				</span>
+			))}
+		</div>
+	);
+};
