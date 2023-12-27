2a6b1f1ac00c6d6824c23e317032af3f0bb0718b
Gilbert Acosta <gisawebdev@gmail.com>
2023-12-26 21:07:26 -0400

    update

--- Android/app/src/main/java/com/example/exerciseandroid/imccalculator/ImcCalculatorActivity.kt
+++ Android/app/src/main/java/com/example/exerciseandroid/imccalculator/ImcCalculatorActivity.kt
@@ -3,33 +3,127 @@ package com.example.exerciseandroid.imccalculator
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import com.example.exerciseandroid.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.slider.RangeSlider
+import java.text.DecimalFormat
 
 class ImcCalculatorActivity : AppCompatActivity() {
 
     private var isMaleSelected: Boolean = true
     private var isFemaleSelected: Boolean = false
+    private var currentHeight: Int = 120
+    private var currentWeight: Int = 70
+    private var currentAge: Int = 50
 
     private lateinit var viewMale: CardView
     private lateinit var viewFemale: CardView
+
+    private lateinit var tvHeight: TextView
+    private lateinit var rsHeight: RangeSlider
+
+    private lateinit var tvWeight: TextView
+    private lateinit var btnSubtractWeight: FloatingActionButton
+    private lateinit var btnPlusWeight: FloatingActionButton
+
+    private lateinit var tvAge: TextView
+    private lateinit var btnSubtractAge: FloatingActionButton
+    private lateinit var btnPlusAge: FloatingActionButton
+
+    private lateinit var btnCalculate: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc_calculator)
         initComponents()
         initListeners()
+        initUI()
     }
 
     private fun initComponents() {
         viewMale = findViewById(R.id.viewMale)
         viewFemale = findViewById(R.id.viewFemale)
+
+        tvHeight = findViewById(R.id.tvHeight)
+        rsHeight = findViewById(R.id.rsHeight)
+
+        tvWeight = findViewById(R.id.tvWeight)
+        btnSubtractWeight = findViewById(R.id.btnSubtractWeight)
+        btnPlusWeight = findViewById(R.id.btnPlusWeight)
+
+        tvAge = findViewById(R.id.tvAge)
+        btnSubtractAge = findViewById(R.id.btnSubtractAge)
+        btnPlusAge = findViewById(R.id.btnPlusAge)
+
+        btnCalculate = findViewById(R.id.btnCalculate)
     }
 
     private fun initListeners() {
-        viewMale.setOnClickListener { setGenderColor() }
-        viewFemale.setOnClickListener { setGenderColor() }
+        viewMale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
+        viewFemale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
+
+        rsHeight.addOnChangeListener { _, value, _ ->
+            val decimalFormat = DecimalFormat("#.##")
+            currentHeight = decimalFormat.format(value).toInt()
+            tvHeight.text = "$currentHeight cm"
+        }
+
+        btnSubtractWeight.setOnClickListener {
+            currentWeight--
+            setWeight()
+        }
+        btnPlusWeight.setOnClickListener {
+            currentWeight++
+            setWeight()
+        }
+        btnSubtractAge.setOnClickListener {
+            currentAge--
+            setAge()
+        }
+        btnPlusAge.setOnClickListener {
+            currentAge++
+            setAge()
+        }
+
+        btnCalculate.setOnClickListener {
+            calculateIMC()
+        }
+    }
+
+    private fun initUI() {
+        setGenderColor()
+        setHeight()
+        setWeight()
+        setAge()
+    }
+
+    private fun setHeight() {
+        tvHeight.text = currentHeight.toString()
+    }
+
+    private fun setWeight() {
+        tvWeight.text = currentWeight.toString()
+    }
+
+    private fun setAge() {
+        tvAge.text = currentAge.toString()
+    }
+
+    private fun changeGender() {
+
+
+        isMaleSelected = !isMaleSelected
+        isFemaleSelected = !isFemaleSelected
     }
 
     private fun setGenderColor() {
@@ -50,5 +144,9 @@ class ImcCalculatorActivity : AppCompatActivity() {
         return ContextCompat.getColor(this, colorReference)
 
 
+    }
+
+    private fun calculateIMC() {
+        val imc = currentWeight / (currentHeight * currentHeight)
     }
 }
